d397bcbf7e634b54f676f8e77ed06b9ec0d81614
fa-bs <0fabiobs@gmail.com>
2024-04-11 00:29:34 -0300

    github upload

---
+++ README.md
@@ -0,0 +1,13 @@
+# Vanilla javaScript ToDoList
+
+Aquele primeiro app desenvolvido tempos atrás com uma repaginada no código, focado em manipulação do DOM com **vanilla.js**.
+
+That first app from a while back, but with a code makeover, focusing on DOM manipulation with **vanilla.js**.
+
+* <https://fa-bs.github.io/vanilla-to-do-list/>
+
+##
+![Javascript](https://img.shields.io/badge/Javascript-F0DB4F?style=for-the-badge&labelColor=black&logo=javascript&logoColor=F0DB4F)
+![HTML](https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white)
+![CSS3](https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white)
+
---
+++ assets/script.js
@@ -0,0 +1,92 @@
+const main = document.querySelector(".main");
+const input = document.querySelector(".input");
+const btnAdd = document.querySelector(".btn-add");
+const btnSave = document.querySelector(".btn-save");
+const msg = document.querySelector(".msg")
+let counter = 0;
+
+//check localStorage
+if (localStorage.getItem('myList')) {
+    main.innerHTML = JSON.parse(localStorage.getItem('myList'))
+    if (main.childNodes.length > 0) {
+        msg.classList.replace("fade-in", "fade-out")
+    }
+}
+
+
+
+function addTask() {
+    if (input.value) {
+        counter++;
+        const date = new Date()
+        const newItem = document.createElement("li")
+        newItem.setAttribute("id", counter)
+        newItem.innerHTML = `<div id="item-${counter}" class="item fade-in">
+        <div class="item-text">${input.value}</div>
+        <div class="item-icon" onclick="checkTask(${counter})"><i id="icon-${counter}" class="bi bi-circle"></i></div>
+        <button class="btn-delete" onclick="deleteTask(${counter})"></i></button>
+        </div>
+        <small style="--delay: .2s" class="date fade-in">${date.toLocaleDateString("pt-BR")} - ${date.toLocaleTimeString()} </small>
+    `
+
+        main.prepend(newItem)
+        // main.append(newItem)
+        input.value = ""
+        msg.classList.replace("fade-in", "fade-out")
+        input.focus()
+    } else {
+        input.focus()
+        input.classList.add("input-attn")
+        setTimeout(() => {
+            input.classList.remove("input-attn")
+        }, 1000);
+    }
+
+}
+
+input.addEventListener("keyup", (event) => {
+    if (event.keyCode === 13) {
+        event.preventDefault()
+        btnAdd.click()
+    }
+});
+
+function checkTask(id) {
+    const li = document.getElementById(id);
+    const item = document.getElementById("item-" + id)
+    const icon = document.getElementById("icon-" + id)
+    item.classList.toggle("item-done")
+
+
+    li.parentNode.appendChild(li)
+    if (icon.classList.contains("bi-check-circle-fill")
+    ) li.parentNode.prepend(li)
+
+    icon.classList.toggle("bi-circle")
+    icon.classList.toggle("bi-check-circle-fill")
+}
+
+function deleteTask(id) {
+    const task = document.getElementById(id)
+    task.classList.add("fade-out")
+    setTimeout(() => {
+        task.remove()
+        main.classList.add("refresh-itens")
+        setTimeout(() => {
+            main.classList.remove("refresh-itens")
+        }, 700);
+        if (main.childNodes.length == 0) {
+            msg.classList.replace("fade-out", "fade-in")
+        }
+    }, 900)
+    input.focus()
+}
+
+//save in localStorage
+const saveList = () => {
+    localStorage.setItem('myList', JSON.stringify(main.innerHTML))
+    btnSave.innerHTML = `<i class="bi bi-save2-fill" ></i>`
+    setTimeout(() => {
+        btnSave.innerHTML = `save <i class="bi bi-save2-fill" ></i>`
+    }, 1000);
+}
